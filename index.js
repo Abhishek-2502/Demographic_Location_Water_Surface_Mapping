c663f04a42a58baff95b4e23e6b8fd218d68c2f7
Abhishek Rajput <abhishek25022004@gmail.com>
2024-05-04 23:36:49 +0530

    Added Content

--- index.js
+++
@@ -1,7 +0,0 @@
-function redirectToMonthPage(year) {
-    const overlay = document.getElementById('pageTransitionOverlay');
-    overlay.classList.remove('hidden'); 
-    setTimeout(() => {
-        window.location.href = `months.html?year=${year}`;
-    }, 100); 
-}
